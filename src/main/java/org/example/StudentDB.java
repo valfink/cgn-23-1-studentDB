ccfde6b86e3dcb003e031d7388f1eb5c1d1dccff
valfink <valentin.finke@gmail.com>
2023-02-02 10:58:45 +0100

    Shorten StudentDB Class Code with Lombok

--- src/main/java/org/example/StudentDB.java
+++ src/main/java/org/example/StudentDB.java
@@ -1,41 +1,32 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDB {
 
     private Map<Integer,Student> studentList;
-
-    public StudentDB(Map<Integer, Student> studentList) {
-        this.studentList = studentList;
-    }
 
     public Map<Integer,Student> getAllStudents(){
         return studentList;
     }
 
-    @Override
-    public String toString() {
-        return "StudentDB{" +
-                "studentList=" + studentList +
-                '}';
-    }
 
     public Student randomStudent(){
         List<Integer> possibleKeys = new ArrayList<>();
         for ( Integer key : studentList.keySet() ) {
             possibleKeys.add( key );
-        };
+        }
         int randomIndex = (int) (Math.random()*possibleKeys.size());
         Integer randomKey = possibleKeys.get(randomIndex);
         return studentList.get(randomKey);
     }
-    /*
-    (1, Valentin)
-    (5, Hamideh)
-    (20, Ana)
-    (100, Daniel)
-    */
 
     public void addStudent(Student newStudent){
         studentList.put(newStudent.getId(), newStudent);
