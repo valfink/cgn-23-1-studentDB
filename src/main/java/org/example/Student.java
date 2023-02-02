a4e5c89a5216e986d99d7fe7e23124a2fb7d24ab
valfink <valentin.finke@gmail.com>
2023-02-02 10:56:08 +0100

    Shorten Student Class Code with Lombok

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,78 +1,26 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
-import java.util.Objects;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     private String name;
     private int id;
     private LocalDate birthday;
 
-
-    public Student(String name, int id, LocalDate birthday) {
-        this.name = name;
-        this.id = id;
-        this.birthday=birthday;
-    }
+    // Constructor with only two arguments, not provided by Lombok
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", birthday=" + birthday +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Student student = (Student) o;
-
-        if (id != student.id) return false;
-        if (!Objects.equals(name, student.name)) return false;
-        return Objects.equals(birthday, student.birthday);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + id;
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        return result;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
     }
 
     public String howLongUntilBirthday() {
